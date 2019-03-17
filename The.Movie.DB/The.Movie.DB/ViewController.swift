3b7d9007b31378f083c44e36ff4499724164102c
Diego Costa Pereira <diegoasp@gmail.com>
2019-03-17 14:33:46 -0300

    project blank

---
+++ The.Movie.DB/The.Movie.DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The.Movie.DB
+//
+//  Created by Diego Costa Pereira on 17/03/19.
+//  Copyright © 2019 Diego Costa Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ The.Movie.DB/The.Movie.DBTests/The_Movie_DBTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Movie_DBTests.swift
+//  The.Movie.DBTests
+//
+//  Created by Diego Costa Pereira on 17/03/19.
+//  Copyright © 2019 Diego Costa Pereira. All rights reserved.
+//
+
+import XCTest
+@testable import The_Movie_DB
+
+class The_Movie_DBTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
